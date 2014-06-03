fe2f74b66ba2f8a65294036a98b2712ffa1dcaa8
Dmitriy Frolov <at@frolov.at>
2014-06-03 14:45:51 +0400

    fucken-fucken-fucken-fonts

--- core/config/environments/production.rb
+++ core/config/environments/production.rb
@@ -1,5 +1,6 @@
 Rails.application.class.configure do
+  config.assets.paths << "#{Rails.root}/app/assets/fonts"
   config.assets.precompile += ['wysihtml5/*']
-  config.assets.paths << Rails.root.join("app", "assets", "fonts")
+  config.assets.precompile += ['fonts/*']
   config.assets.precompile += %w( .svg .eot .woff .ttf )
 end
