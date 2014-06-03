60c10c1acd5c7f273229959aa266c720ab3ad994
Dmitriy Frolov <at@frolov.at>
2014-06-03 14:30:48 +0400

    fonts

--- core/config/environments/production.rb
+++ core/config/environments/production.rb
@@ -1,4 +1,5 @@
 Rails.application.class.configure do
   config.assets.precompile += ['wysihtml5/*']
-  config.assets.precompile += ['fonts/*']
+  config.assets.paths << Rails.root.join("app", "assets", "fonts")
+  config.assets.precompile += %w( .svg .eot .woff .ttf )
 end
